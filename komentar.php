93a3f8046873b42fc48eebf1f63ce9456f6be51a
zulaykhaalmira <43508955+zulaykhaalmira@users.noreply.github.com>
2018-10-02 16:30:38 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,13 @@
-<?php
+<form method="POST">
+	Komentar <input type="textbox" name="komen"><br>
+	<input type="submit" name="submit" value="Kirim">
+</form>
 
+<?php
+	if (isset($_POST['submit'])) {
+		$komen = $_POST['komen'];
+		$hasil = str_word_count($komen);
+
+		echo "Jumlah Kata : $hasil";
+	}
 ?>
