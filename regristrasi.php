6d99eb126f3f226ea85fe9dd2c0065d4dfadc2ac
zulaykhaalmira <43508955+zulaykhaalmira@users.noreply.github.com>
2018-10-02 16:31:32 +0700

    Update regristrasi.php
    proses dan registrasinya disatuin

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,91 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
+<form action="" method="POST">
+	<center>
+		<table>
+			<tr>
+				<td>NIM</td>
+				<td><input type="text" name="nim" maxlength="10" required=""></td>
+			</tr>
+			<tr>
+				<td>Nama</td>
+				<td><input type="text" name="nama" maxlength="25" required=""></td>
+			</tr>
+			<tr>
+				<td>Email</td>
+				<td><input type="email" name="email" maxlength="30" required=""></td>
+			</tr>
+			<tr>
+				<td><input type="submit" name="submit" value="submit"></td>
+			</tr>
+		</table>
+</center>
+</form>
 
+<?php
+ 	$servername = "localhost";
+ 	$nim = "root";
+ 	$nama = "";
+ 	$db = "jurnal";
+ 	$con = new mysqli($servername, $nim, $nama, $db);
 
+ 	if($con==false){
+ 		die("Koneksi Gagal" . $con->connect_error);
+ 	
+ 	}
+ 	if(isset($_POST['submit'])){
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama'];
+	$email = $_POST['email'];
 
-</body>
-</html>
+	$sql = "INSERT INTO regist (nim,nama,email) values ('$nim','$nama','$email')";
+	if (mysqli_query($con, $sql)){
+		echo "<center>Database sudah masuk</center>";
+	}else{
+		echo "<center>ERROR<center>";
+	}
+	}
+?><form action="" method="POST">
+	<center>
+		<table>
+			<tr>
+				<td>NIM</td>
+				<td><input type="text" name="nim" maxlength="10" required=""></td>
+			</tr>
+			<tr>
+				<td>Nama</td>
+				<td><input type="text" name="nama" maxlength="25" required=""></td>
+			</tr>
+			<tr>
+				<td>Email</td>
+				<td><input type="email" name="email" maxlength="30" required=""></td>
+			</tr>
+			<tr>
+				<td><input type="submit" name="submit" value="submit"></td>
+			</tr>
+		</table>
+</center>
+</form>
+
+<?php
+ 	$servername = "localhost";
+ 	$nim = "root";
+ 	$nama = "";
+ 	$db = "jurnal";
+ 	$con = new mysqli($servername, $nim, $nama, $db);
+
+ 	if($con==false){
+ 		die("Koneksi Gagal" . $con->connect_error);
+ 	
+ 	}
+ 	if(isset($_POST['submit'])){
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama'];
+	$email = $_POST['email'];
+
+	$sql = "INSERT INTO regist (nim,nama,email) values ('$nim','$nama','$email')";
+	if (mysqli_query($con, $sql)){
+		echo "<center>Database sudah masuk</center>";
+	}else{
+		echo "<center>ERROR<center>";
+	}
+	}
+?>
